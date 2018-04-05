0d498ffb06ebd6dbbe67b6a192f50bb3599a0835
Dibya Das <dibyadascool@gmail.com>
2018-04-05 20:25:21 +0530

    add props to Current

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      status: "Just Started"
+      status: "Just Started",
+      flag: 0,
+      curract: "this is new"
     };
+
+    setInterval(() => {
+      if(this.state.flag === 0){
+        this.setState({
+          status: "new",
+          flag: 1,
+          curract: "sdgdfg"
+        });
+      }
+      else {
+        this.setState({
+          status: "old",
+          flag: 0,
+          curract: "dfnbm"
+        });
+      }
+    },200);
+
   }
 
   render() {
@@ -18,7 +38,7 @@ class App extends Component {
           <div>Smart, Neuro-Fuzzy system based app that takes care of your mental and social health</div>
         </header>
         <div className="status-message"> {this.state.status} </div>
-        <Current />
+        <Current activity={this.state.curract} />
         <div className="text">
           Something is really good!
         </div>
--- frontend/src/components/Current.jsx
+++ frontend/src/components/Current.jsx
@@ -4,15 +4,37 @@ import '../css/Current.css';
 class Current extends Component {
     constructor() {
         super();
-        this.state = {
-            activity: 'Dummy variable',
-        };
+        // this.props = ['activity'];
+        // this.state = {
+        //     activity: 'Dummy variable',
+        // };
+        // var tempact = this.props.activity;
+        // this.state = {
+        //     flag: 0,
+        //     activity: tempact,
+        // };
+        // setInterval(() => {
+        //     if(this.state.flag === 0){
+        //       this.setState({
+        //         activity: "new",
+        //         flag: 1,
+        //       });
+        //       this.props.activity = "sdgsd";
+        //     }
+        //     else {
+        //       this.setState({
+        //         activity: "old",
+        //         flag: 0,
+        //       });
+        //       this.props.activity = "eryhth";
+        //     }
+        //   },200);
     }
 
     render() {
         return (
             <div className='Current'>
-                Current Activity :- {this.state.activity}
+                Current Activity :- {this.props.activity}
             </div>
         )
     }
